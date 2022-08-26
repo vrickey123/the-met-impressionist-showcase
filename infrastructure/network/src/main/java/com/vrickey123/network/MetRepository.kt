1e66c531900288b9dfb5926820c0aab1ff1a8940
Vince Rickey <vince.rickey@gmail.com>
2022-08-26 02:49:05 -0400

    Implement MetRepositoryImpl

--- infrastructure/network/src/main/java/com/vrickey123/network/MetRepository.kt
+++ infrastructure/network/src/main/java/com/vrickey123/network/MetRepository.kt
@@ -2,24 +2,28 @@ package com.vrickey123.network
 
 import com.vrickey123.model.api.MetObject
 import com.vrickey123.model.api.MetSearchResult
-import kotlinx.coroutines.flow.Flow
 
 interface MetRepository {
     val metNetworkClient: MetNetworkClient
-
-    // One day this could be implemented to return data from a local persistent store
-    // such as Room or a SQL Lite Database.
     // val metLocalDataStore: MetLocalDataStore
 
-    fun search(
+    /**
+     * One-shot request to The Met search API.
+     * @return [MetSearchResult]
+     * */
+    suspend fun search(
         query: String,
         hasImages: Boolean,
         tags: List<String>
-    ): Flow<MetSearchResult>
+    ): Result<MetSearchResult>
 
-    fun fetchMetObject(objectID: Int): Flow<MetObject>
+    /**
+     * One-shot request to The Met Object API.
+     * @return [MetObject]
+     * */
+    suspend fun fetchMetObject(objectID: Int): Result<MetObject>
 
     // One day this could be implemented to return data from a local persistent store
-    // such as Room or a SQL Lite Database.
-    //fun getMetObject()
+    // such as Room or a SQL Lite Database emit time that it is updated.
+    //fun getMetObject(): Flow<Result<MetObject>>
 }
--- infrastructure/network/src/main/java/com/vrickey123/network/MetRepositoryImpl.kt
+++ infrastructure/network/src/main/java/com/vrickey123/network/MetRepositoryImpl.kt
@@ -2,18 +2,47 @@ package com.vrickey123.network
 
 import com.vrickey123.model.api.MetObject
 import com.vrickey123.model.api.MetSearchResult
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
 
-class MetRepositoryImpl(override val metNetworkClient: MetNetworkClient) : MetRepository {
-    override fun search(
+class MetRepositoryImpl(
+    override val metNetworkClient: MetNetworkClient,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+) : MetRepository {
+
+    override suspend fun search(
         query: String,
         hasImages: Boolean,
         tags: List<String>
-    ): Flow<MetSearchResult> {
-        TODO("Not yet implemented")
+    ): Result<MetSearchResult> = withContext(ioDispatcher) {
+        handleResponse(metNetworkClient.search(query, hasImages, tags))
     }
 
-    override fun fetchMetObject(objectID: Int): Flow<MetObject> {
-        TODO("Not yet implemented")
+
+    override suspend fun fetchMetObject(objectID: Int): Result<MetObject> =
+        withContext(ioDispatcher) {
+            handleResponse(metNetworkClient.fetchMetObject(objectID))
+        }
+
+    /**
+     * Convenience function for transforming an HTTP Response into a Kotlin [Result]
+     * */
+    private fun <T> handleResponse(response: Response<T>): Result<T> {
+        val body = response.body() // Kotlin smart cast is stupid
+        return if (response.isSuccessful && body != null) {
+            Result.success(body)
+        } else {
+            Result.failure(Throwable(response.message()))
+        }
     }
+
+    // One day this could be implemented to return data from a local persistent store
+    // such as Room or a SQL Lite Database emit time that it is updated. Various caching
+    // strategies such as e-tags or updatedAfter filters could be used in the repository to
+    // return remote or local data. Apollo GraphQL has similar niceties.
+    // fun getMetObject(objectID): Flow<Result<MetObject>> {
+    //   metLocalDataStore.getMetObject(objectID)
+    // }
 }

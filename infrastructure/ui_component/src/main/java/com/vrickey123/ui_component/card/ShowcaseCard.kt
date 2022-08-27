281cecb2bd590107e9e832f7a740be8f8580f311
Vince Rickey <vince.rickey@gmail.com>
2022-08-27 17:12:54 -0400

    Update modules to Android API 33

---
+++ infrastructure/ui_component/src/main/java/com/vrickey123/ui_component/card/ShowcaseCard.kt
@@ -0,0 +1,19 @@
+package com.vrickey123.ui_component.card
+
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ShowcaseCard() {
+    ElevatedCard() {
+
+    }
+}
+
+@Preview
+@Composable
+fun ShowcaseCardPreview() {
+    Text(text = "hello world")
+}

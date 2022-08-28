ff064d244a69894b843894b39ac65dade58c26e0
Vince Rickey <vince.rickey@gmail.com>
2022-08-28 04:04:44 -0400

    Update getPaintings flow implementation

--- feature/showcase/src/main/java/com/vrickey123/showcase/ui/ShowcaseViewModelImpl.kt
+++ feature/showcase/src/main/java/com/vrickey123/showcase/ui/ShowcaseViewModelImpl.kt
@@ -1,5 +1,6 @@
 package com.vrickey123.showcase.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.vrickey123.model.api.MetObject
@@ -21,6 +22,76 @@ class ShowcaseViewModelImpl @Inject constructor(
     companion object {
         const val QUERY = "impressionism"
         val TAGS = listOf<String>("impressionism")
+        val IDS = listOf<Int>(
+            437133,
+            671456,
+            436155,
+            437654,
+            839045,
+            482132,
+            438816,
+            283238,
+            437135,
+            436965,
+            438003,
+            437384,
+            435621,
+            437299,
+            436122,
+            436964,
+            436139,
+            438815,
+            436950,
+            436141,
+            436144,
+            435702,
+            438821,
+            437835,
+            437310,
+            436004,
+            436162,
+            437984,
+            436002,
+            436149,
+            435991,
+            436528,
+            436575,
+            438016,
+            436001,
+            438001,
+            438820,
+            436174,
+            435967,
+            438015,
+            436536,
+            437868,
+            435875,
+            435802,
+            435739,
+            436095,
+            436840,
+            436123,
+            436534,
+            435868,
+            436529,
+            436530,
+            437175,
+            437980,
+            436951,
+            438136,
+            488978,
+            435880,
+            435626,
+            436548,
+            437640,
+            437896,
+            436140,
+            483301,
+            436970,
+            437890,
+            435881,
+            436553
+        )
     }
 
     override val mutableState: MutableStateFlow<ShowcaseUIState> =
@@ -34,19 +105,19 @@ class ShowcaseViewModelImpl @Inject constructor(
     }
 
     override fun getPaintings() {
-        flow<Result<MetSearchResult>> { emit(metRepository.search(QUERY, true, TAGS)) }
-            .map { metSearchResult ->
+        flow<Result<List<MetObject>>> {
+            metRepository.search(QUERY, true, TAGS).map { metSearchResult ->
                 // make a fetchMetObject API call for each objectID in the MetSearchResult
-                metSearchResult.getOrThrow().objectIDs.subList(0, 4).map { metRepository.fetchMetObject(it) }
+                metSearchResult.objectIDs.map {
+                    // transform a List<Result<MetObject> to a List<MetObject>
+                    metRepository.fetchMetObject(it).getOrThrow()
+                }
             }
-            .buffer() // creates a buffer of a List<Result<MetObject>> for previous api calls above
-            .map { listOfResult ->
-                // transform a List<Result<MetObject> to a List<MetObject>
-                listOfResult.map { it.getOrThrow() }
-            }
-            .onEach { mutableState.emit(ShowcaseUIState(data = it)) }
-            .catch { mutableState.emit(ShowcaseUIState(error = it)) }
-            .launchIn(viewModelScope)
+        }.onEach {
+            mutableState.emit(reduce(it))
+        }.catch {
+            mutableState.emit(reduce(Result.failure(it)))
+        }.launchIn(viewModelScope)
     }
 
     override fun reduce(result: Result<List<MetObject>>): ShowcaseUIState {

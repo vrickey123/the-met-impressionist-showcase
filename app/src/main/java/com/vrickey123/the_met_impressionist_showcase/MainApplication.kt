6bcb5e319a69df4985acec6eac05a1417f26ea5b
Vince Rickey <vince.rickey@gmail.com>
2022-08-28 02:10:45 -0400

    Add Coil ImageLoaderFactory and image cache

--- app/src/main/java/com/vrickey123/the_met_impressionist_showcase/MainApplication.kt
+++ app/src/main/java/com/vrickey123/the_met_impressionist_showcase/MainApplication.kt
@@ -1,7 +1,18 @@
 package com.vrickey123.the_met_impressionist_showcase
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication: Application()
+class MainApplication: Application(), ImageLoaderFactory {
+
+    // Coil Image Cache. It is referenced under the hood by AsyncImage's
+    // LocalContext.current.imageLoader.
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .crossfade(true)
+            .build()
+    }
+}

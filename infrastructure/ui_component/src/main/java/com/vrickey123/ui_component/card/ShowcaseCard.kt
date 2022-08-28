ac513d8c6f3634e6fb1b3a8efe526d988d2d3e08
Vince Rickey <vince.rickey@gmail.com>
2022-08-28 18:55:00 -0400

    Code formatting

--- infrastructure/ui_component/src/main/java/com/vrickey123/ui_component/card/ShowcaseCard.kt
+++ infrastructure/ui_component/src/main/java/com/vrickey123/ui_component/card/ShowcaseCard.kt
@@ -8,12 +8,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.vrickey123.model.ImageData
 import com.vrickey123.ui_component.image.AsyncImageComponent
 
@@ -27,8 +23,16 @@ fun ShowcaseCard(
     ElevatedCard(modifier.fillMaxWidth()) {
         AsyncImageComponent(imageData = imageData)
         Column(Modifier.padding(horizontal = 20.dp)) {
-            Text(modifier = Modifier.padding(top = 10.dp), text = title, style = MaterialTheme.typography.headlineMedium)
-            Text(modifier = Modifier.padding(vertical = 10.dp), text = artistDisplayName, style = MaterialTheme.typography.labelLarge)
+            Text(
+                modifier = Modifier.padding(top = 10.dp),
+                text = title,
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Text(
+                modifier = Modifier.padding(vertical = 10.dp),
+                text = artistDisplayName,
+                style = MaterialTheme.typography.labelLarge
+            )
         }
     }
 }
